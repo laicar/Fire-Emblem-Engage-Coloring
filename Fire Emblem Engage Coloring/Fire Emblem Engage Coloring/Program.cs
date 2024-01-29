1eab9598627edcd32252c57933427f0d9ef36e16
laicar <carole.laitong@gmail.com>
2024-01-29 20:59:27 +0100

    Properly target 6.0, update Skia
    The was made for .Net 8.0, so it broke when I was asked to make it target 6.0.

--- Fire Emblem Engage Coloring/Fire Emblem Engage Coloring/Program.cs
+++ Fire Emblem Engage Coloring/Fire Emblem Engage Coloring/Program.cs
@@ -1,9 +1,16 @@
 ﻿//C# Program to apply Fire Emblem Engage palette files to corresponding sprites by u/eclogia
 
-using static Fire_Emblem_Engage_Coloring.EngageColorer;
-
-if(Environment.GetCommandLineArgs().Length != 4) {
-    Console.WriteLine("Please provide the following folders' path as arguments in order: raw (red scale) sprites, palettes, output");
-    return;
+namespace Fire_Emblem_Engage_Coloring;
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        if (args.Length != 3)
+        {
+            Console.WriteLine("Please provide the following folders' path as arguments in order: raw (red scale) sprites, palettes, output");
+            return;
+        }
+        EngageColorer.ProcessFolders(args[0], args[1], args[2]);
+        Console.WriteLine("Finished.");
+    }
 }
-ProcessFolders(Environment.GetCommandLineArgs()[1], Environment.GetCommandLineArgs()[2], Environment.GetCommandLineArgs()[3]);
